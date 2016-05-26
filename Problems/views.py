fb52792bfc37f9a2c9375030534e3c7a465d5e9f
Tyler Holden <tholden@math.toronto>
2016-05-26 16:12:05 -0400

    PDFLATEX for escape lt and gt

--- Problems/views.py
+++ Problems/views.py
@@ -562,6 +562,8 @@ def latexify_string(string):
     ret_string = re.sub(r'(<br>)+', r'\\', ret_string)
     ret_string = re.sub(r'<b>(.*)</b>', r'\\textbf{\1}', ret_string)
     ret_string = re.sub(r'<i>(.*)</i>', r'\\emph{\1}', ret_string)
+    ret_string = re.sub(r'&lt;', r'<', ret_string)
+    ret_string = re.sub(r'&gt;', r'>', ret_string)
 
     return ret_string
 
@@ -575,7 +577,7 @@ def create_latex_source(question_list, title):
 
     # Note that amssymb is the only package that we should need.
     preamble = """\\documentclass{{article}}
-\\usepackage{{amssymb,amsthm}}
+\\usepackage{{amssymb,amsthm, amsmath}}
 
 \\newenvironment{{solution}}{{\\renewcommand\qedsymbol{{$\\blacksquare$}}
   \proof[Solution]
