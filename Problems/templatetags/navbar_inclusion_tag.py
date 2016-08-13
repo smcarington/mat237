5c5f52fc74be2f1253656916b2f09938fe4a42de
Tyler Holden hbox <tholden@math.toronto.edu>
2016-08-13 15:49:55 -0400

    Logout redirect

--- Problems/templatetags/navbar_inclusion_tag.py
+++ Problems/templatetags/navbar_inclusion_tag.py
@@ -12,7 +12,10 @@ register = template.Library()
 @register.inclusion_tag('Problems/navbar_inclusion_tag.html', takes_context = True)
 def navbar_inclusion_tag(context):
     ps = ProblemSet.objects.all().order_by('pk')
-    return {'problem_sets': ps, 'request': context.request, 'site_name': settings.SITE_NAME }
+    return {'problem_sets': ps, 
+            'request': context.request, 
+            'site_name': settings.SITE_NAME,
+            'logout_page': settings.LOGOUT_REDIRECT_URL}
 
 @register.simple_tag
 def check_active(request, view_name):
