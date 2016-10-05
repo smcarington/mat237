beed50d94751d8a58d2d76c25d0c0e0792cae647
Tyler Holden hbox <tholden@math.toronto.edu>
2016-10-05 13:12:33 -0400

    Added several command utilities

--- Problems/management/commands/upload_marks.py
+++ Problems/management/commands/upload_marks.py
@@ -9,12 +9,12 @@ from Problems.models import StudentMark, ExemptionType
 import logging
 
 class Command(BaseCommand):
-    """ A command for taking CRA data and updating student grades
-        Expects a comma separated list
-        username, email, student_number, first_name, last_name, cra_score
+    """ A command for uploading marks. Expects a comma separated list
+        username, email, student_number, first_name, last_name, score
     """
 
     def add_arguments(self, parser):
+        parser.add_argument('category')
         parser.add_argument('filename')
         parser.add_argument('--log', dest='log')
 
@@ -22,27 +22,27 @@ class Command(BaseCommand):
         if options['log']:
             logging.basicConfig(filename=options['log'], level=logging.DEBUG)
 
-        exemption, created = ExemptionType.objects.get_or_create(name="CRA", out_of="100")
+        exemption, created = ExemptionType.objects.get_or_create(name=options['category'])
 
         with open(options['filename']) as e_file:
             lines = [line.strip().split(',') for line in e_file]
 
         for user in lines:
             try:
-                [username, email, student_number, first_name, last_name, cra_score] = [item.strip() for item in user]
+                [username, email, student_number, first_name, last_name, score] = [item.strip() for item in user]
                 
                 user = User.objects.get(username=username, 
                                         first_name=first_name,
                                         last_name=last_name)
               
-                print('Updating CRA score for {} {}. Score {}'.format(user.first_name, user.last_name, cra_score))
+                print('Updating {} score for {} {}. Score {}'.format(exemption.name, user.first_name, user.last_name, score))
 
-                if not cra_score:
-                    cra_score = 0
+                if not score:
+                    score = 0
 
                 stud_mark, created = StudentMark.objects.get_or_create(user=user, category=exemption)
-                stud_mark.set_score(round(float(cra_score)))
+                stud_mark.set_score(round(float(score)))
             except Exception as e:
-                string = 'ERROR: {first} {last} with username: {username} at {email}. \n Exception: {e}'.format(first=first_name, last=last_name, username=username, email=email, e=e)
+                string = 'ERROR: {first} {last} with username: {username}. \n Exception: {e}'.format(first=first_name, last=last_name, username=username, email=email, e=e)
                 print(string)
                 logging.error(string)
