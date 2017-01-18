7775ea765713f5dea8484efd71d7e919babae09b
kreizhn <kreizhn@gmail.com>
2017-01-18 17:09:54 -0500

    First pass for sort key

--- Problems/management/commands/dump_student_dict.py
+++ Problems/management/commands/dump_student_dict.py
@@ -25,12 +25,48 @@ class Command(BaseCommand):
         # Marks all students as inactive
         students=User.objects.filter(is_staff=False, is_active=not options['all']).prefetch_related('info').order_by('last_name')
 
+        # Generate sorting code as follows: 
+        # 023_M15_0102-12 = 23 student globally, 15th student with M last name, 
+        # Tutorial 0102, 12th in that tutorial alphabetically
+       
+        glob_count    = 0
+        tutorial_list = StudentInfo.objects.values('tutorial').distinct()
+        tut_dict      = {tut_name: 0 for tut_name in tutorial_list}
+        cur_letter    = 'A'
+        let_number    = 0
+
+        # Get current number of students in class, to use for number padding
+        padding = len(str(students.count()))
+        template = "{glob:0>{pad}d}_{let}{let_num}_{tutorial}-{tut_num}}"
+
         for student in students:
             try:
+                stud_tutorial  = student.info.tutorial
+                stud_last_name = student.last_name
+
+                global_count   += 1
+                tutorial[stud_tutorial] += 1
+
+                #Check to see if last name letter has changed
+                if stud_last_name[0] == cur_letter:
+                    let_number += 1
+                else:
+                    cur_letter = stud_last_name[0]
+                    let_number = 1
+
+                sort_string = template.format(
+                                glob = global_count,
+                                pad  = padding,
+                                let  = cur_letter,
+                                let_num = let_number,
+                                tutorial = stud_tutorial,
+                                tut_num = tut_dict[stud_tutorial])
+
                 print(json.dumps({'first_name': student.first_name,
                                   'last_name' : student.last_name,
                                   'student_number': student.info.student_number,
-                                  'tutorial': student.info.tutorial})
+                                  'tutorial': student.info.tutorial,
+                                  'sort': sort_string})
                      )
             
             except Exception as e:
